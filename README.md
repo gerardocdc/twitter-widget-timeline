43ed3ea4e76643c8a45a025a66fdc0b78befc35f
Gerardo Colorado Diaz-Caneja <gerardocdc@gmail.com>
2012-12-18 15:58:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-twitter-widget-timeline
+Twitter Timeline Widget
 =======================
 
-Creates a widget for Twitter showing a user's timeline
+Purpose
+-------
+Creates a widget for Twitter showing a user's timeline.
+
+Configuration
+-------------
+You must first create a widget for your Twitter account.
+https://twitter.com/settings/widgets/
+Once you have created the widget, you must obtain the widget's Id from the HTML code to insert in your site. They consist normally of a number (like 1234567890).
+
+Usage
+-----
+	$twitterWidget = new TwitterWidgetTimeline();
+	$twitterWidget->setUsername("my.twitter.username");
+	$twitterWidget->setWidgetId("1234567890";
+	$twitterWidget->render();
+
+Copyright
+---------
+Creative Commons CC-BY-SA License (http://creativecommons.org/licenses/by-sa/3.0/)
+Copyright (c) 2012 Diaz-Caneja Consultores
+
+Contact
+--------
+Gerardo Colorado Diaz-Caneja   gcdiazcaneja@diaz-caneja-consultores.com
+Github: https://github.com/gerardocdc/twitter-widget-timeline (feel free to contribute)
---
+++ TwitterWidgetTimeline.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Class TwitterWidgetTimeline
+ * Creates a widget for Twitter showing a user's timeline
+ * https://twitter.com/settings/widgets/
+ *
+ * @author		Gerardo Colorado Diaz-Caneja <gcdiazcaneja@diaz-caneja-consultores.com>
+ * @link		http://www.diaz-caneja-consultores.com
+ * @link        https://github.com/gerardocdc/twitter-widget-timeline
+ * @copyright	Copyright (c) 2012 Diaz-Caneja Consultores
+ * @license		Creative Commons CC-BY-SA License (http://creativecommons.org/licenses/by-sa/3.0/)
+ * @version     1.0
+ * @filesource
+ */
+
+/**
+ * Class TwitterWidgetTimeline
+ * Creates a widget for Twitter showing a user's timeline
+ * https://twitter.com/settings/widgets/
+ *
+ * Usage:
+ * <code>
+ *      $twitterWidget = new TwitterWidgetTimeline();
+ *		$twitterWidget->setUsername("my.twitter.username");
+ *		$twitterWidget->setWidgetId("1234567890";
+ *		$twitterWidget->render();
+ * </code>
+ *
+ * @package		Twitter
+ * @version     1.0
+ * @since		Version 1.0
+ */
+class TwitterWidgetTimeline
+{
+	/*--------------------------------------------
+	|             V A R I A B L E S             |
+	============================================*/
+
+	/**
+	 * Twitter username
+	 *
+	 * @var string
+	 */
+	private $username;
+
+	/**
+	 * Twitter widget id
+	 *
+	 * @var integer
+	 */
+	private $widgetId;
+
+	/*--------------------------------------------
+	|           C O N S T R U C T O R           |
+	============================================*/
+
+	/**
+	 * Constructor
+	 */
+	function __construct()
+	{
+	}
+
+	/*--------------------------------------------
+	|      G E T T E R S / S E T T E R S        |
+	============================================*/
+
+	/**
+	 * @param string $username
+	 */
+	public function setUsername($username)
+	{
+		$this->username = $username;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUsername()
+	{
+		return $this->username;
+	}
+
+	/**
+	 * @param int $widgetId
+	 */
+	public function setWidgetId($widgetId)
+	{
+		$this->widgetId = $widgetId;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getWidgetId()
+	{
+		return $this->widgetId;
+	}
+
+	/*--------------------------------------------
+	|        C L A S S   M E T H O D S          |
+	============================================*/
+
+	/**
+	 * Renderizes and creates the Twitter Widget Timeline
+	 */
+	public function render()
+	{
+		$code = "<a class=\"twitter-timeline\"";
+		$code .= "href=\"https://twitter.com/".$this->getUsername()."\"";
+		$code .= "data-widget-id=\"".$this->getWidgetId()."\">";
+		$code .= "Tweets por @".$this->getUsername()."</a>";
+		$code .= "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>";
+
+		echo $code;
+	}
+}
